996bef04879c747bb7b8025db143732f7c43af11
PI3RRE003 <vitorcruz.dev@gmail.com>
2026-01-28 15:19:14 -0300

    feat: criando a classe clima para o consumo da API

---
+++ app/model/clima.rb
@@ -0,0 +1,14 @@
+class Clima
+  attr_reader :cidade, :temperatura, :condicao, :umidade, :vento
+  def initialize(cidade, temperatura, condicao, umidade, vento)
+    @cidade = cidade
+    @temperatura = temperatura
+    @condicao = condicao
+    @umidade = umidade
+    @vento = vento
+  end
+
+  def perigoso?
+    @temperatura > 35
+  end
+end
