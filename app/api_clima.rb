87bcdd81637b6bd3d622657d998bdd31a422d67f
PI3RRE003 <vitorcruz.dev@gmail.com>
2026-01-28 15:48:46 -0300

    env e gems

--- app/api_clima.rb
+++ app/api_clima.rb
@@ -1,11 +1,12 @@
 require 'httparty'
+require 'dotenv/load'
 
 class Apiclima
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-    API_KEY = "CHAVE_API"
+    API_KEY = ENV["CHAVE_API"]
 
     def self.buscar(cidade)
-        resposta_api = HTTParty.get("#{BASE_URL}?=#{cidade}&appid=#{API_KEY}&units=metric&lang=pt-br")
+        resposta_api = HTTParty.get("#{BASE_URL}?q=#{cidade}&appid=#{API_KEY}&units=metric&lang=pt-br")
 
         if resposta_api.success?
           dados = resposta_api.parsed_response
@@ -18,6 +19,9 @@ class Apiclima
             dados["wind"]["speed"]
           )
         else
+          puts "=== ERRO NA REQUISIÇÃO ==="
+          puts "Status Code: #{resposta_api.code}" # Ex: 401, 404, 429
+          puts "Corpo do Erro: #{resposta_api.body}"
           nil
         end
     end
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
-require 'time'
+require 'dotenv/load'
+require_relative 'model/clima'
+require_relative 'api_clima'
 
 get '/' do 
-    erb :layout
+    if params[:city]
+      p @clima = Apiclima.buscar(params[:city])
+
+      if @clima.nil?
+        @erro = "Cidade não encontrada, tente novamente."
+      end
+    end
+    erb :index
 end
 
 post '/clima' do 
+
 end 
